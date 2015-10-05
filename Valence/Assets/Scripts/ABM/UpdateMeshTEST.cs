2e3aeefed32c99465a30e589e8880ea3a254eae7
ZachSullivan <zach.r.sullivan@gmail.com>
2015-10-05 13:22:49 -0400

    Agents Added + Realtime Map Updates

---
+++ Valence/Assets/Scripts/ABM/UpdateMeshTEST.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+using Pathfinding;
+
+public class UpdateMeshTEST : MonoBehaviour {
+
+
+
+	void OnGUI(){
+		if (GUI.Button (new Rect (30, 30, 200, 30), "Update World")) {
+			//GameObject obstical = GameObject.FindWithTag("bound");
+						
+			foreach (GameObject obstcale in GameObject.FindGameObjectsWithTag("prop")) {
+				AstarPath.active.UpdateGraphs(obstcale.gameObject.GetComponent<Collider>().bounds);
+			}
+
+
+		}
+	}
+
+}
