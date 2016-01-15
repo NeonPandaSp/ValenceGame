b943dcc512651eac1a3dcfeba324d7ca3f96edb8
TylerDinardo <tylerdinardo@L-CB4111-29.CUNET.CARLETON.CA>
2016-01-15 18:35:24 -0500

    Explore: Changed Obstacle Tile Referencing
    Now it can be something other than 1x1
    
    A-ha-ha, a-ha-ha, Gloria, how's it gonna go down?
    Will you meet him on the main line, or will you catch him on the
    rebound?
    Will you marry for the money, take a lover in the afternoon?
    Feel your innocence slipping away, don't believe it's comin' back soon

--- Valence/Assets/ObstacleScript.cs
+++ Valence/Assets/ObstacleScript.cs
@@ -4,9 +4,18 @@ using System.Collections;
 public class ObstacleScript : MonoBehaviour {
 	public ExploreMode_GameController _GameController;
 	public int myTileType;
+	public int myWidth;
+	public int myHeight;
+	public Vector2 myOrigin;
 	// Use this for initialization
 	void Start () {
 		_GameController = FindObjectOfType<ExploreMode_GameController>();
-		_GameController.tiles [(int)transform.position.x,(int)transform.position.z] = myTileType;
+
+		for (int i =(int) myOrigin.x; i < (int) myOrigin.x+myWidth; i++) {
+			for( int j =(int) myOrigin.y; j < (int) myOrigin.y+myHeight; j++ ){
+				if( i >= 0 && i < _GameController.mapSize && j >= 0 && j < _GameController.mapSize )
+					_GameController.tiles [i,j] = myTileType;
+			}
+		}
 	}
 }
