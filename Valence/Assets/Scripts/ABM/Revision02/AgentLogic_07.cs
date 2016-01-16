a4ff8f25d2e087d477ce71be2ece747b08fb08f0
ZachSullivan <zach.r.sullivan@gmail.com>
2016-01-16 17:56:50 -0500

    Build: Agents now consume food
    Agents now consume food, they feature a new hunger value system. Where
    by, each agent will have a 0-100% of hunger (0=full, 100 = starving). At
    25, 50 and 75% a series of dice are thrown (1, 3, 6 respectively). If a
    true is rolled on any of these dice, then the agent will move towards
    the food source and collect 5% of all food currently stored there. they
    will then return to their last known behaviour

--- Valence/Assets/Scripts/ABM/Revision02/AgentLogic_07.cs
+++ Valence/Assets/Scripts/ABM/Revision02/AgentLogic_07.cs
@@ -30,6 +30,19 @@ public class AgentLogic_07 : MonoBehaviour {
     //Check if the current agent has been assigned to the correct list
     bool populateList;
 
+    //Check if the agent has food, if yes then consume the food, else start starving
+    bool hasFood;
+
+    //Check if the agent is hungry, if true the go to a known foodsource
+    bool isHungry;
+
+    //Agent stores 5% of food found in storage facility
+    public float foodStored;
+
+    //0-100 int which holds the agent's current hunger level. 0% = full, 100% = starving 
+    public int hungerValue;
+
+    int amount = 0;
 
     AIFollow_07 aiFollow;
     GameController gameController;
@@ -55,14 +68,15 @@ public class AgentLogic_07 : MonoBehaviour {
     public jobSubState jobState;
 
     //Variable used to save the last active state
+    //Todo, change this to be a list which saves the last ie. 5 states... 
     public agentState currentState;
 
     void Start(){
 
+        //Init the agent's hunger value to 0 when spawned (they shouldnt be hungry at start)
+        hungerValue = 0;
         
         aiFollow = GetComponent<AIFollow_07>();
-
-       
         wanderWaypoints = new List<Vector3>();
 
         //Populate the following waypoints when an agent is spawned
@@ -70,35 +84,59 @@ public class AgentLogic_07 : MonoBehaviour {
             wanderWaypoints.Add(new Vector3(Random.Range(0, 160), 0, Random.Range(0, 120)));
         }
 
+        //Get all storage waypoints when the agent is spawned, agent should know a known food source at spawn
         storageWaypoints = new List<GameObject>(GameObject.FindGameObjectsWithTag("StorageWaypoint"));
 
         gameController = GameObject.FindGameObjectWithTag("GameController").GetComponent<GameController>();
 
+        //init all waypoint index's to 0 and set the storage waypoint indext to a random value within the scope of the storage list
         workWaypointIndex = 0;
         wanderWaypointIndex = 0;
         storageWaypointIndex = Random.Range(0, storageWaypoints.Count);
         
+        //Agent should  have no current assigned target
         assignedTarget = false;
+
+        //Agent should not have populated any worker lists
         populateList = false;
 
         //When an agent spawns, he should start by wandering
         aState = agentState.Wandering;
 
-	}
+        //Once everything has been set, begin consuming resources
+        BeginFeeding();
+    }
 
 	void Update() {
-		switch (aState) {
+
+     
+        
+
+        switch (aState) {
 
             case agentState.Wandering:
-
+                //Save the current state
+                currentState = aState;
+                //Set the current target to move towards
                 aiFollow.target = wanderWaypoints[wanderWaypointIndex];
+
+                //remove this agent from all worker lists
+                //Todo do this with a function call
+                gameController.farmerList.Remove(this.gameObject);
+
                 break;
 		    case agentState.Hungry:
+                
                 aiFollow.target = storageWaypoints[storageWaypointIndex].transform.position;
+
+                //remove this agent from all worker lists
+                //Todo do this with a function call
+                gameController.farmerList.Remove(this.gameObject);
+
                 break;
 			
 		    case agentState.Working:
-               
+                currentState = aState;
                 //Agent can be assigned various jobs each with their own behaviour
                 switch (jobState) {
                     case jobSubState.Farmer:
@@ -106,8 +144,8 @@ public class AgentLogic_07 : MonoBehaviour {
                         //Set the agent to move towards the farm waypoints
                         aiFollow.target = workWaypoints[workWaypointIndex].transform.position;
 
-                        //Add the agent to the list of farmers
-                        
+                        //Add the agent to the list of farmers 
+                        //Todo: Should update this to a function which when called takes a passed value of the substate and adds this agent to the correct list
                         if (!populateList)
                         {
                             gameController.farmerList.Add(this.gameObject);
@@ -152,6 +190,113 @@ public class AgentLogic_07 : MonoBehaviour {
         }
 	}
 
+
+
+    //When called, the agent will begin consuming resources needed to live (food, water)
+    public void BeginFeeding() {
+
+        Debug.Log("Agent has begun consuming resources...");
+        //Repeat the function ConsumeResource, for 1 second, every 1 second
+        InvokeRepeating("ConsumeResource", 1.0f, 1.0f);
+    }
+            
+    //When the agent's hunger % reaches a critial amount, then switch the current state of the agent to hunger state (search for food)
+    void ConsumeResource() {
+
+        //Todo: need to update this function to take into account the current food the agent has collected...
+        //ie. once they collect 100 food ONLY start increasing the hungerValue once he has finished feeding on that 100 food
+
+        switch (hasFood) {
+            case true:
+
+                //Check that there still is food stored
+                if (foodStored != 0) {
+                    //if yes then decrease the amount of food stored on the agent
+                    foodStored--;
+                }
+                else {
+                    //if there is no more food stored on the agent, then start starving the agent
+                    hasFood = false;
+                }
+
+            break;
+            case false:
+                //check if the agent's hunger% has reached 25%, otherwise keep increasing the hungerValue
+                //Todo update this if statement to check boolean, boolean set based on probability
+                // if (hungerValue >= 25) {
+
+                //Bool set if true then change state to hungry
+                if (isHungry) { 
+                    //if yes then set the agent's state to hungry, but first save the previous state..
+                    //so the agent may return to it after feeding
+
+                    aState = agentState.Hungry;
+                }
+                else {
+                    //Increase the current hunger value of the agent by 1
+                    //Todo: need to incorporate a probability factor that is affected by the current hunger value %, higher % = higher probablility to change Astate to hungry
+                    hungerValue++;
+
+                    //Throw a series of dice at each milestone 25,50,75% hunger, if any dice roll true, then move to the food source
+                    switch (hungerValue) {
+                        case 25:
+
+                            isHungry = Choose();
+                        break;
+
+                        case 50:
+                            for (amount = 0; amount < 3; amount++)
+                                if (!isHungry) {
+                                    isHungry = Choose();
+                                }
+                        break;
+
+                        case 75:
+                            for (amount = 0; amount < 6; amount++)
+                                if (!isHungry) {
+                                    isHungry = Choose();
+                                }
+                    break;
+                    }
+                    
+                //Choose(hungerValue);
+                }
+            break;
+            default:
+                Debug.Log("Default reached in AgentLogic_07 - ConsumeResource function");
+            break;
+        }
+
+       
+
+    }
+
+    bool Choose() {
+
+        //Calc the bayes theorm on probability of hunger after 100 tries at 20%
+        /*float probability = 0.20f;
+        float tempVal = 0;
+        float tempVal2 = 0;
+        Debug.Log("Probability: " + probability);
+        for (int i = 1; i <= 100; i++) {
+            tempVal = ((i * probability) * Mathf.Pow((1 - probability), i - 1));
+            tempVal2 += tempVal;
+            Debug.Log("Probability: " + tempVal2);
+        }*/
+
+        if (Random.Range(1.0f, 100.0f) < 50) {
+            // will be true 10% of the time
+            Debug.Log("True");
+            return true;
+        }
+        else {
+            Debug.Log("False");
+            return false;
+
+        }
+
+    }
+
 	bool PointInsideSphere(Vector3 point, Vector3 center, float radius) {
 		print ("Dis" + Vector3.Distance(point, center));
 		print ("inside: " + (Vector3.Distance(point, center) < radius));
@@ -174,9 +319,26 @@ public class AgentLogic_07 : MonoBehaviour {
         }   else if (aState == agentState.Hungry){
 
             //Reset hunger level or start replenishing
+            //Todo slowly start decreasing hunger value
+            hungerValue = 0;
+            isHungry = false;
+
+            //Give the agent 10% of the current food stored
+            //Todo need to figureout a beter method for this algorithm
+            foodStored = 0.10f * gameController.food;
+            gameController.food -= foodStored;
+
+            //Tell the agent that they current have food and should not starve
+            hasFood = true;
+
+            //Todo need to check if there is actually food available for the agent to feed on
+            //ADD HERE
+
+
+
             //Go back to previous task
             aState = currentState;
-
+            
             //Need to Update the wait length to take into account the % of hunger missing (Higher hunger missing = longer wait time)
 
         }
