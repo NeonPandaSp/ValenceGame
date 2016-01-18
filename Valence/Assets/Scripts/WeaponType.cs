c1f41dfad6cc9b2f1b6eb0b43be891d3c09e96d8
ZachSullivan <zach.r.sullivan@gmail.com>
2016-01-18 17:27:14 -0500

    Build: Added procedural weapon generator

---
+++ Valence/Assets/Scripts/WeaponType.cs
@@ -0,0 +1,132 @@
+﻿using UnityEngine;
+using System.Collections;
+[System.Serializable]
+
+public class WeaponType {
+
+    public string typeName;
+
+    //public int cost;
+
+    public int rare;
+
+    public int damageModifier;
+    public int range;
+    public float accuracy;
+    public int soundRange;
+
+    // Use this for initialization
+    void Start () {
+	
+	}
+
+    public WeaponType(string n, int a)
+    {
+        typeName = n;
+        //rare = a;
+
+        damageModifier = getWeaponDamage();
+        range = getWeaponRange();
+        accuracy = getWeaponAccuracy();
+        soundRange = getSoundRange();
+    }
+
+    public int getWeaponDamage(){
+        if ((typeName == "Pistol") || (typeName == "pistol")) {
+            return Random.Range(1, 5);
+        }
+        else if ((typeName == "Rifle") || (typeName == "rifle")) {
+            return Random.Range(5, 10);
+        }
+        else if ((typeName == "Shotgun") || (typeName == "shotgun")) {
+            return Random.Range(5, 10);
+        }
+        else if ((typeName == "Melee") || (typeName == "melee")) {
+            return Random.Range(1, 4);
+        }
+        else{
+            Debug.Log("Weapon Damage Error");
+            return 0;
+        }
+    }
+
+    public int getWeaponRange()
+    {
+        if ((typeName == "Pistol") || (typeName == "pistol"))
+        {
+            return Random.Range(1, 5);
+        }
+        else if ((typeName == "Rifle") || (typeName == "rifle"))
+        {
+            return Random.Range(5, 10);
+        }
+        else if ((typeName == "Shotgun") || (typeName == "shotgun"))
+        {
+            return Random.Range(1, 3);
+        }
+        else if ((typeName == "Melee") || (typeName == "melee"))
+        {
+            return 1;
+        }
+        else
+        {
+            Debug.Log("Weapon Range Error");
+            return 0;
+        }
+    }
+
+    public float getWeaponAccuracy()
+    {
+        if ((typeName == "Pistol") || (typeName == "pistol"))
+        {
+            return Random.Range(0.1f, 0.5f);
+        }
+        else if ((typeName == "Rifle") || (typeName == "rifle"))
+        {
+            return Random.Range(0.5f, 0.9f);
+        }
+        else if ((typeName == "Shotgun") || (typeName == "shotgun"))
+        {
+            return Random.Range(0.5f, 0.9f);
+        }
+        else if ((typeName == "Melee") || (typeName == "melee"))
+        {
+            return Random.Range(0.1f, 0.9f);
+        }
+        else
+        {
+            Debug.Log("Weapon Accuracy Error");
+            return 0;
+        }
+    }
+
+    public int getSoundRange()
+    {
+        if ((typeName == "Pistol") || (typeName == "pistol"))
+        {
+            return Random.Range(0, 7);
+        }
+        else if ((typeName == "Rifle") || (typeName == "rifle"))
+        {
+            return Random.Range(0, 12);
+        }
+        else if ((typeName == "Shotgun") || (typeName == "shotgun"))
+        {
+            return Random.Range(0, 12);
+        }
+        else if ((typeName == "Melee") || (typeName == "melee"))
+        {
+            return 0;
+        }
+        else
+        {
+            Debug.Log("Weapon Sound Range Error");
+            return 0;
+        }
+    }
+
+    // Update is called once per frame
+    void Update () {
+	
+	}
+}
---
+++ Valence/Assets/WeaponGenerator.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class WeaponGenerator : MonoBehaviour {
+
+    public List<WeaponType> weaponList;
+
+    //Weapon Type to be spawned
+    public string myType;
+
+    //Rarity of the weapon
+    public int wRare;
+
+    public WeaponType wType;
+    // Use this for initialization
+    void Start () {
+        weaponList = new List<WeaponType>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void OnGUI()
+    {
+        if (GUI.Button(new Rect(Screen.width - 700, 10, 150, 20), "Generate Weapon"))
+        {
+            wType = new WeaponType(myType, wRare);
+            weaponList.Add(wType);
+        }
+    }
+
+}
