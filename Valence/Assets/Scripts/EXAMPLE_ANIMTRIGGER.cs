429f950f4136b954c2241e68e001ab6438696639
ZachSullivan <zach.r.sullivan@gmail.com>
2016-02-28 20:16:16 -0500

    Test: Added Combat Anim Test READ DESC
    +Character holds various weapons during animation
    +Animation will trigger weapon flash at fire point
    +Added 2 new scripts weaponlogic to play the particle system &
    EXAMPLE_ANIMTRIGGER to play the according weapon fire

---
+++ Valence/Assets/Scripts/EXAMPLE_ANIMTRIGGER.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EXAMPLE_ANIMTRIGGER : MonoBehaviour {
+
+    //List of all weapons that can have a muzzle flash
+    public GameObject Shotgun, Pistol, Rifle;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+
+    public void FireWeapon() {
+        Rifle.GetComponent<WeaponLogic>().FireWeapon();
+        Pistol.GetComponent<WeaponLogic>().FireWeapon();
+        Shotgun.GetComponent<WeaponLogic>().FireWeapon();
+
+    }
+}
---
+++ Valence/Assets/Scripts/WeaponLogic.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class WeaponLogic : MonoBehaviour {
+
+    public List<ParticleSystem> particles;
+
+    //This is a debug bool to manually trigger the weapon fire flash
+    public bool trigger;
+
+	// Use this for initialization
+	void Start () {
+        particles = new List<ParticleSystem>(GetComponentsInChildren<ParticleSystem>());
+
+        trigger = false;
+        
+	}
+
+    void Update() {
+        if (trigger) {
+            FireWeapon();
+        }
+    }
+
+	// When Called weapon plays a muzzle flash
+	public void FireWeapon () {
+        foreach (ParticleSystem MuzzleFlash in particles) {
+            MuzzleFlash.Play();
+        }
+
+        trigger = false;
+    }
+}
