2ee566e0a17179037e70dcb4bd4ad2551b1c88ac
TylerDinardo <tylerdinardo@L-CB4111-32.CUNET.CARLETON.CA>
2016-03-01 17:07:25 -0500

    Cargo Explore Map Implemente

--- Valence/Assets/ObstacleScript.cs
+++ Valence/Assets/ObstacleScript.cs
@@ -7,15 +7,18 @@ public class ObstacleScript : MonoBehaviour {
 	public int myWidth;
 	public int myHeight;
 	public Vector2 myOrigin;
+	public BoxCollider myBox;
 	// Use this for initialization
 	void Start () {
 		_GameController = FindObjectOfType<ExploreMode_GameController>();
+		myBox = GetComponentInChildren<BoxCollider> ();
+		myWidth = (int) myBox.size.x;
+		myHeight = (int) myBox.size.z;
 		updateTileMap ();
-
 	}
 
 	void updateTileMap(){
-		myOrigin = new Vector2 (transform.position.x, transform.position.z);
+		myOrigin = new Vector2 (Mathf.CeilToInt( transform.position.x-(myWidth/2) ) , Mathf.CeilToInt(transform.position.z-(myHeight/2)));
 		for (int i =(int) myOrigin.x; i < (int) myOrigin.x+myWidth; i++) {
 			for( int j =(int) myOrigin.y; j < (int) myOrigin.y+myHeight; j++ ){
 				if( i >= 0 && i < _GameController.mapSize && j >= 0 && j < _GameController.mapSize )
