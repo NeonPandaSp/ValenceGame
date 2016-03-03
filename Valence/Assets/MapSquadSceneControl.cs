125b9dd8a0f44f552b6a435f8c196e12e055d51b
TylerDinardo <Tyler.Dinardo@Carleton.ca>
2016-03-03 12:24:36 -0500

    Explore Mode Map loading changes

--- Valence/Assets/MapSquadSceneControl.cs
+++ Valence/Assets/MapSquadSceneControl.cs
@@ -5,12 +5,12 @@ using System.Collections.Generic;
 
 public class MapSquadSceneControl : MonoBehaviour {
 	public GameObject mapCan, squadCan;
-	public int mapIndex = 4;
+	public int mapIndex;
 
 	public List<serialAgent> myParty;
 	// Use this for initialization
 	void Start () {
-	
+
 	}
 	
 	// Update is called once per frame
@@ -30,6 +30,7 @@ public class MapSquadSceneControl : MonoBehaviour {
 
 	public void loadExploreMap(){
 		PlayerData oldData = PlayerDataManager.playerDataManager.loadSaveData ();
+		int mapIndex = mapCan.GetComponent<MapSelectionScript> ().selectedMap;
 		foreach (serialAgent pM in squadCan.GetComponent<SquadSelectionScript>().myParty) {
 			myParty.Add( pM );
 		}
