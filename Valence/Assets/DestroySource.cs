d2968ef974babe102b1fbc4eaf8bf4fd52c7642b
TylerDinardo <tylerdinardo@gmail.com>
2016-03-07 16:17:11 -0500

    Build Mode: Early Tool Tips Implementation

---
+++ Valence/Assets/DestroySource.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DestroySource : MonoBehaviour {
+	public GameObject nextTip;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public void ObjToDestroy( GameObject myObj ){
+		Destroy (myObj);
+	}
+
+	public void Sequencer(){
+		nextTip.SetActive (true);
+		this.gameObject.SetActive (false);
+	}
+}
---
+++ Valence/Assets/Scripts/hoverComponent.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class hoverComponent : MonoBehaviour {
+	public GameObject myTooltip;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+	void OnMouseEnter() {
+		Debug.Log ("Enter");
+		myTooltip.gameObject.SetActive (true);
+	}
+	void OnMouseOver() {
+		Debug.Log ("OVER");
+	}
+	void OnMouseExit() {
+		Debug.Log ("Exit");
+		myTooltip.gameObject.SetActive (false);
+	}
+}
