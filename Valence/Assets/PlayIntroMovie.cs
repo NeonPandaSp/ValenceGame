62164175ebb17658170bb4473b79f96b8b663556
ZachSullivan <zach.r.sullivan@gmail.com>
2016-03-07 17:59:33 -0500

    Menu: Added intro video

---
+++ Valence/Assets/PlayIntroMovie.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+
+
+public class PlayIntroMovie : MonoBehaviour {
+
+    public MovieTexture movie;
+
+	// Use this for initialization
+	void Start () {
+        GetComponent<RawImage>().texture = movie as MovieTexture;
+        movie.Play();
+        movie.loop = true;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        
+    }
+}
