d7d02a5688cd096f5cc2b071929c71e356d52fde
ZachSullivan <zach.r.sullivan@gmail.com>
2016-03-22 14:56:38 -0400

    Build: Added drag-able pause menu

---
+++ Valence/Assets/Scripts/UIDrag.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class UIDrag : MonoBehaviour {
+
+    float OffsetX;
+    float OffsetY;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    public void BeginDrag() {
+        OffsetX = transform.position.x - Input.mousePosition.x;
+        OffsetY = transform.position.y - Input.mousePosition.y;
+    }
+
+    public void Drag() {
+        transform.position = new Vector3(OffsetX + Input.mousePosition.x, OffsetY + Input.mousePosition.y);
+    }
+}
