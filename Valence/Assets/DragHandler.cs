4696ac00448ae7f698f5c272509485a13eb40ed9
ZachSullivan <zach.r.sullivan@gmail.com>
2016-03-22 19:46:42 -0400

    Build: Drag and drop building UI added
    Up next, use this UI to assign settlers

--- Valence/Assets/DragHandler.cs
+++ Valence/Assets/DragHandler.cs
@@ -5,15 +5,21 @@ using UnityEngine.EventSystems;
 public class DragHandler : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDragHandler {
 
     Vector3 startPos;
-    
+    public Transform parentToReturnTo = null;
+
     public void OnBeginDrag(PointerEventData eventData) {
         Debug.Log("OnBeginDrag: " + gameObject);
 
         startPos = this.transform.position;
+
+        parentToReturnTo = this.transform.parent;
+        this.transform.SetParent(this.transform.parent.parent.parent);
+
+        GetComponent<CanvasGroup>().blocksRaycasts = false;
     }
 
     public void OnDrag(PointerEventData eventData) {
-        Debug.Log("OnDrag: " + gameObject);
+        //Debug.Log("OnDrag: " + gameObject);
 
         Vector3 mouse = eventData.position;
         mouse.z = 30.0f;
@@ -28,5 +34,7 @@ public class DragHandler : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndD
         Debug.Log("OnEndDrag: " + gameObject);
 
         this.transform.position = startPos;
+        this.transform.SetParent(parentToReturnTo);
+        GetComponent<CanvasGroup>().blocksRaycasts = true;
     }
 }
---
+++ Valence/Assets/Scripts/DropZone.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.EventSystems;
+
+public class DropZone : MonoBehaviour, IDropHandler, IPointerEnterHandler, IPointerExitHandler {
+
+    public void OnPointerEnter(PointerEventData eventData) {
+
+    }
+
+    public void OnPointerExit(PointerEventData eventData) {
+    }
+
+    public void OnDrop(PointerEventData eventData) {
+        Debug.Log(eventData.pointerDrag.name +" was droped on to " + gameObject.name);
+
+        DragHandler d = eventData.pointerDrag.GetComponent<DragHandler>();
+        if (d != null) {
+            d.parentToReturnTo = this.transform.FindChild("AgentList");
+        }
+    }
+
+
+}
