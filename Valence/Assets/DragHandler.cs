4f4e1f59dad6e3456cc8124b7fdc39f60e3932c2
ZachSullivan <zach.r.sullivan@gmail.com>
2016-03-22 18:44:07 -0400

    Build: Added Drag functionality to Building UI

---
+++ Valence/Assets/DragHandler.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.EventSystems;
+
+public class DragHandler : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDragHandler {
+
+    Vector3 startPos;
+    
+    public void OnBeginDrag(PointerEventData eventData) {
+        Debug.Log("OnBeginDrag: " + gameObject);
+
+        startPos = this.transform.position;
+    }
+
+    public void OnDrag(PointerEventData eventData) {
+        Debug.Log("OnDrag: " + gameObject);
+
+        Vector3 mouse = eventData.position;
+        mouse.z = 30.0f;
+        mouse = Camera.main.ScreenToWorldPoint(mouse);
+
+        this.transform.position = mouse;
+
+    }
+
+    public void OnEndDrag(PointerEventData eventData) {
+
+        Debug.Log("OnEndDrag: " + gameObject);
+
+        this.transform.position = startPos;
+    }
+}
