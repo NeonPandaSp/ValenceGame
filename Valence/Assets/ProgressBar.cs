b071e2c89c91eabc5e2939793a46fadfc7ad0e75
ZachSullivan <zach.r.sullivan@gmail.com>
2016-03-22 12:37:34 -0400

    Build: Added idle icons for non working F. Agents

--- Valence/Assets/ProgressBar.cs
+++ Valence/Assets/ProgressBar.cs
@@ -39,8 +39,8 @@ public class ProgressBar : MonoBehaviour {
 		currentTime = myBuildingScript.resourceProgress;
 
 		float barScale = Remap (currentTime, 0.0f, productionCompleteTime, 0.0f, 1.0f);
-		//Debug.Log (currentTime);
-		//Debug.Log (barScale);
+        //Debug.Log (currentTime);
+        Debug.Log (barScale);
 		//if (myBuildingScript.powered) {
 		this.gameObject.transform.localScale = new Vector3( barScale, this.gameObject.transform.localScale.y, this.gameObject.transform.localScale.z);
 		//}
--- Valence/Assets/UIFaceCamera.cs
+++ Valence/Assets/UIFaceCamera.cs
@@ -10,7 +10,7 @@ public class UIFaceCamera : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		transform.LookAt (transform.position + myCamera.transform.rotation * Vector3.back,
+		transform.LookAt (transform.position + myCamera.transform.rotation * Vector3.forward,
 		                 myCamera.transform.rotation * Vector3.up);
 	}
 }
