97415df6ccf06b78f0ccfba4b8040fbac0a709bb
ZachSullivan <zach.r.sullivan@gmail.com>
2016-03-24 15:15:02 -0400

    Build: Drag and Drop to assign agents to a farm!
    Todo: drag and drop to unassign

--- Valence/Assets/NonWorkingAgentList.cs
+++ Valence/Assets/NonWorkingAgentList.cs
@@ -7,16 +7,19 @@ public class NonWorkingAgentList : MonoBehaviour {
 
     public GameController _myGameController;
     public List<GameObject> unAssignedAgents;
+    public List<GameObject> workWaypoints;
+    List<int> indexMemory = new List<int>();
     public GameObject agentProfile;
-    //public DragHandler dragHandler;
+    bool stateChange = true;
 
 	// Use this for initialization
 	void Start () {
         GameObject gameControllerObject = GameObject.FindGameObjectWithTag("GameController");
         _myGameController = gameControllerObject.GetComponent<GameController>();
 
-        unAssignedAgents = _myGameController.unAssignedPopulation;
-
+        foreach (Transform child in this.gameObject.transform.root.FindChild("Workwaypoints")) {
+            workWaypoints.Add(child.gameObject);
+        }
         //GameObject test = Instantiate(agentProfile, agentProfile.transform.position, agentProfile.transform.rotation) as GameObject;
         //test.transform.SetParent(this.transform, false);
 
@@ -24,6 +27,7 @@ public class NonWorkingAgentList : MonoBehaviour {
             GameObject newobject = Instantiate(agentProfile, agentProfile.transform.position, agentProfile.transform.rotation) as GameObject;
             newobject.transform.SetParent(this.transform, false);
             newobject.GetComponentInChildren<Text>().text = agent.GetComponent<AgentLogic_07>().firstLastName;
+            unAssignedAgents.Add(newobject);
             //gameObject.transform;
         }
         
@@ -31,6 +35,39 @@ public class NonWorkingAgentList : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-        
+        foreach (GameObject agent in unAssignedAgents) {
+            Debug.Log("Root is: " + agent.transform.parent.tag);
+
+            if (agent.transform.parent.tag == "Assigned") {
+                ChangeAgentWorkState(unAssignedAgents.IndexOf(agent));
+                
+                Debug.Log("Current index " + unAssignedAgents.IndexOf(agent));
+            }
+
+        }
     }
+
+    void ChangeAgentWorkState(int index) {
+        GameObject agent = _myGameController.population[index];
+
+        if (!indexMemory.Contains(index)) {
+
+            switch (this.gameObject.transform.root.GetComponent<BuildingScript>().bType.typeName) {
+                case "Farm":
+                string childLocation = "/" + this.gameObject.transform.root.name + "/Workwaypoints";
+                agent.GetComponent<AgentLogic_07>().workWaypoints = workWaypoints;
+
+                agent.GetComponent<AgentLogic_07>().aState = AgentLogic_07.agentState.Working;
+                agent.GetComponent<AgentLogic_07>().jobState = AgentLogic_07.jobSubState.Farmer;
+                stateChange = false;
+                Debug.Log("FARMER NOW!");
+                break;
+            }
+            Debug.Log("Agent Name: " + agent.GetComponent<AgentLogic_07>().firstLastName);
+        } else {
+            indexMemory.Add(index);
+        }
+    }
+
 }
+
